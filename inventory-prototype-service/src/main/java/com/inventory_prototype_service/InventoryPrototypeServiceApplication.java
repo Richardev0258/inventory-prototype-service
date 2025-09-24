cad3fd9e974c413b33da68fe402fb585eaeeb0d2
Ricardo Andrés Marín Pinilla <ricardomarin0258@gmail.com>
2025-09-23 20:02:54 -0500

    First commit

---
+++ inventory-prototype-service/src/main/java/com/inventory_prototype_service/InventoryPrototypeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inventory_prototype_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryPrototypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryPrototypeServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-prototype-service/src/test/java/com/inventory_prototype_service/InventoryPrototypeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory_prototype_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryPrototypeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
